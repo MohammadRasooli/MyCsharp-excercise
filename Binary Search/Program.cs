a0ae4174b8b28b2cb023005ba10cd86c75264c45
Mohammad <MohammadRasooli.j@gmail.com>
2023-08-09 07:03:54 +0330

    MyCsharp Exercises

---
+++ Binary Search/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+var mynum = 4;
+
+Array.Sort(array: numbers);
+
+int index = Array.BinarySearch(numbers, mynum); // جستجوی دودویی عدد مورد نظر
+
+if (index >= 0)
+{
+    Console.WriteLine(numbers[index] + " = " + index);
+}
+else
+{
+    Console.WriteLine("-1");
+}
