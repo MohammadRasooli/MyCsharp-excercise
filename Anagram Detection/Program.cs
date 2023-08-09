5bf6c0e2cd8d0866a7de99916b64a2ac9718c46c
Mohammad <MohammadRasooli.j@gmail.com>
2023-08-09 10:40:00 +0330

    MyCsharp Exercises

---
+++ Anagram Detection/Program.cs
@@ -0,0 +1,20 @@
+﻿string str = "integral";
+string str2 = "triangle";
+
+Char[] charArray = str.ToCharArray();
+Char[] charArray2 = str2.ToCharArray();
+
+Array.Sort(charArray);
+Array.Sort(charArray2);
+
+string check = new string(charArray);
+string check2 = new string(charArray2);
+
+if (check == check2)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
