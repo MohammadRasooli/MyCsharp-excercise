895ed058ffa8be53ef1e366eb99636d359600613
Mohammad <MohammadRasooli.j@gmail.com>
2023-08-09 09:32:32 +0330

    MyCsharp Exercises

---
+++ Find the Duplicate Number/Program.cs
@@ -0,0 +1,18 @@
+﻿
+int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+bool check = false;
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (numbers.Count(x => x == numbers[i]) == 2)
+    {
+        Console.WriteLine($"{numbers[i]} is Duplicate.");
+        i = numbers.Length;
+        check = true;
+    }
+}
+if (!check)
+{
+    Console.WriteLine("-1");
+}
