752975556d80cf4fdefbcf930525fe51395b8d2e
Mohammad <MohammadRasooli.j@gmail.com>
2023-08-09 07:08:58 +0330

    MyCsharp Exercises

---
+++ Palindrome/Program.cs
@@ -0,0 +1,24 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter a Number:");
+        string num = Console.ReadLine();
+        var reverse = Reverse(num);
+        if (num == reverse)
+        {
+            Console.WriteLine("yes");
+        }
+        else
+        {
+            Console.WriteLine("No");
+        }
+    }
+    public static string Reverse(string get)
+    {
+        Char[] charArray = get.ToCharArray();
+        Array.Reverse(charArray);
+        string str = new string(charArray);
+        return str;
+    }
+}
