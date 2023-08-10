c39c7d9b0b0e58ede34f6542efd9ae792ed52df5
Mohammad <MohammadRasooli.j@gmail.com>
2023-08-10 13:12:14 +0330

    MyCsharp Exercises

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Enter Number : ");
+int repetitions = Convert.ToInt32(Console.ReadLine());
+Console.Write("0, 1, 1, ");
+
+int n1 = 1;
+int n2 = 1;
+int sum = 0;
+
+for (int i = 1; i < repetitions; i++)
+{
+    sum = n1 + n2;
+    n1 = n2;
+    n2 = sum;
+
+
+    Console.Write(n2 + ", ");
+}
