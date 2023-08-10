c5f57992885e998a013ffe8d6d7d76a233d4f033
Mohammad <MohammadRasooli.j@gmail.com>
2023-08-10 04:49:11 +0330

    MyCsharp Exercises

---
+++ Maximum Subarray Problem/Program.cs
@@ -0,0 +1,21 @@
+﻿List<int> nums = new List<int> { -1, -2, 5, -4, 8, -2, 1, -3 };
+
+int maxEndingHere = nums[0];
+int maxSoFar = nums[0];
+int startIndex = 0;
+int endIndex = 0;
+
+for (int i = 1; i < nums.Count; i++)
+{
+    maxEndingHere = Math.Max(nums[i], maxEndingHere + nums[i]);
+    if (maxEndingHere == nums[i])
+    {
+        startIndex = i;
+    }
+    if (maxEndingHere > maxSoFar)
+    {
+        maxSoFar = maxEndingHere;
+        endIndex = i;
+    }
+}
+Console.WriteLine($"Start Index: {startIndex}\nEnd Index: {endIndex}\nMax Sum: {maxSoFar}");
