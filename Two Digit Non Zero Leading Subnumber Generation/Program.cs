c6f47f28b72b1cca7b7195e1e8d2e4aa1d2113aa
Mohammad <MohammadRasooli.j@gmail.com>
2023-08-13 10:31:26 +0330

    MyCsharp Exercises

---
+++ Two Digit Non Zero Leading Subnumber Generation/Program.cs
@@ -0,0 +1,21 @@
+﻿int sum = 0;
+for (int i = 112; i < 999; i++)
+{
+    string toString = i.ToString();
+
+    if (toString[0] == toString[1] || toString[0] == toString[2])
+    {
+        if (!toString.Contains("0") && toString.Count(x => x == toString[0]) != 3)
+        {
+            sum++;
+        }
+    }
+    else if (toString[1] == toString[2])
+    {
+        if (!toString.Contains("0") && toString.Count(x => x == toString[0]) != 3)
+        {
+            sum++;
+        }
+    }
+}
+Console.WriteLine(sum);
