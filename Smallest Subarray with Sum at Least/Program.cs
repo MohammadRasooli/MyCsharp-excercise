73a1eb770e5eaa8614814be2891c328823c2023c
Mohammad <MohammadRasooli.j@gmail.com>
2023-08-18 20:33:49 +0330

    MyCsharp Exercises

---
+++ Smallest Subarray with Sum at Least/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] numbers = new int[] { 1, 2, 3, 4, 5 };
+int amountGiven = 8;
+int sum = 0;
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (sum <= amountGiven)
+    {
+        Console.Write(numbers[i] + " ");
+        sum += numbers[i];
+
+        if (sum + numbers[i + 1] > amountGiven)
+        {
+            i = numbers.Length;
+        }
+    }
+}
