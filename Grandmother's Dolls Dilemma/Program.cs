b8bee9877c791b51ab6dc513222357b00c18d7b3
Mohammad <MohammadRasooli.j@gmail.com>
2023-09-03 03:51:00 +0330

    MyCsharp Exercises

---
+++ Grandmother's Dolls Dilemma/Program.cs
@@ -0,0 +1,18 @@
+﻿int numbersOfGrandmasDolls = Convert.ToInt32(Console.ReadLine());
+int totalScream = 2;
+int boy = 1;
+int girl = 1;
+for (int i = numbersOfGrandmasDolls; i > 1; i--)
+{
+    if (i > boy)
+    {
+        totalScream++;
+        boy = i;
+    }
+    else if (i < girl)
+    {
+        totalScream++;
+        girl = i;
+    }
+}
+Console.WriteLine(totalScream);
