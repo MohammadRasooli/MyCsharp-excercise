820f6afeee5673fbb2af1aa5a04b93ef25a0b573
Mohammad <MohammadRasooli.j@gmail.com>
2023-09-03 03:52:21 +0330

    MyCsharp Exercises

--- Grandmother's Dolls Dilemma/Program.cs
+++ Grandmother's Dolls Dilemma/Program.cs
@@ -1,4 +1,11 @@
-﻿int numbersOfGrandmasDolls = Convert.ToInt32(Console.ReadLine());
+﻿/*
+The grandmother has bought dolls for her two grandchildren, a boy and a girl.
+he boy screams if he sees a doll bigger than any he has seen before,
+and the girl screams if she sees a doll smaller than any she has seen before.
+The grandmother needs to present the dolls in a way that minimizes the number of screams from her grandchildren.
+*/
+
+int numbersOfGrandmasDolls = Convert.ToInt32(Console.ReadLine());
 int totalScream = 2;
 int boy = 1;
 int girl = 1;
