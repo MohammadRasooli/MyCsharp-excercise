8935a9f10d6ae98afeb55a88802f164b1bb312d8
Mohammad <MohammadRasooli.j@gmail.com>
2024-11-17 18:04:32 +0330

    updated code

--- Rock Paper Scissors/Program.cs
+++ Rock Paper Scissors/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Microsoft.VisualBasic;
 
 namespace RockPaperScissors
 {
@@ -6,62 +7,59 @@ namespace RockPaperScissors
     {
         static void Main(string[] args)
         {
-            bool continuee = true;
+            bool loop = false;
             do
             {
-                Console.Clear();
-                var randomSelection = random();
-                Console.WriteLine("Enter Rock, Paper or Scissors: ");
-                var input = Console.ReadLine().ToUpper();
-                Console.WriteLine($"PC Selection {randomSelection}");
-                Console.WriteLine($"Player Selection {input}");
-                Console.WriteLine(game(randomSelection, input));
-                Console.WriteLine("Do you want to play again?(Y/N) ");
-                var again = Console.ReadLine().ToLower();
-                while (again != "y" && again != "n")
-                {
-                    Console.WriteLine("An error has occurred. Please only use (y or n).");
-                    again = Console.ReadLine().ToLower();
-                }
-                if (again == "n")
-                {
-                    continuee = false;
-                }
-            }
-            while (continuee);
+                var botSelect = Bot();
+                var playerSelect = Player();
 
-            Console.WriteLine("I hope you enjoy, bye bye :)");
+                Console.WriteLine($"Bot pick : {botSelect}");
+                Compare(botSelect, playerSelect);
+
+                Console.WriteLine("Do you like do it again?(Y/N) : ");
+                var again = Console.ReadLine().ToUpper();
+
+                loop = again != "Y";
+            } while (!loop);
+
+            Console.WriteLine("Thanks for playin ^-^");
         }
-        static string random()
+        static string Bot()
         {
-            Random generator = new Random();
-            int number = generator.Next(1, 4);
-            switch (number)
-            {
-                case 1:
-                    return "ROCK";
-                case 2:
-                    return "PAPER";
-                case 3:
-                    return "SCISSORS";
-            }
-            return null;
+            Random rnd = new Random();
+
+            string[] bot = ["rock", "paper", "scissors"];
+            int rndNumber = rnd.Next(0, 3);
+            string rndSelect = bot[rndNumber];
+
+            return rndSelect;
         }
-        static string game(string pc, string player)
+        static string Player()
         {
-            if (pc == "ROCK" && player == "ROCK" || pc == "PAPER" && player == "PAPER" || pc == "SCISSSORS" && player == "SCISSSORS")
-            {
-                return "its a draw!";
-            }
-            if (pc == "ROCK" && player == "PAPER" || pc == "PAPER" && player == "SCISSORS" || pc == "SCISSORS" && player == "ROCK")
-            {
-                return "you won!";
-            }
-            if (pc == "PAPER" && player == "ROCK" || pc == "SCISSORS" && player == "PAPER" || pc == "ROCK" && player == "SCISSORS")
-            {
-                return "you lose!";
-            }
-            return null;
+            Console.WriteLine("Enter rock, paper, scissors :");
+            var playerSelect = Console.ReadLine().ToLower();
+
+            return playerSelect;
         }
+        static void Compare(string bot, string player)
+        {
+            if (bot == player)
+            {
+                Console.WriteLine("its a draw!");
+            }
+            else if (bot == "rock" && player == "paper" || bot == "paper" && player == "scissors" || bot == "scissors" && player == "rock")
+            {
+                Console.WriteLine("you won!");
+            }
+            else if (bot == "paper" && player == "rock" || bot == "scissors" && player == "paper" || bot == "rock" && player == "scissors")
+            {
+                Console.WriteLine("you lose!");
+            }
+            else
+            {
+                Console.WriteLine("Something wrong!");
+            }
+        }
+
     }
 }
